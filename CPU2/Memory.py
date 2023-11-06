60cd0730fe35c67a921dd678ac59305d49f0e16a
Luca Guerini <lucaguerini912@gmail.com>
2023-11-06 11:43:42 -0500

    Made the data in the file more type-safe.

--- CPU2/Memory.py
+++ CPU2/Memory.py
@@ -1,18 +1,18 @@
 from dataclasses import dataclass
-from typing import List, Literal
+from typing import List, Literal, TypeVar, Generic
 #Purpose: Move memory from the start list to the end list
 #def DMA(start:List,end:List):
 #Implement later
-
+T = TypeVar('T')
 @dataclass
 class Memory:
-    data: List[int]
+    data: List[T]
     def DATA(self):
         return self.data
-    def write(self, int start, data):
+    def write(self, start: int, data: T):
         pass
     def read(self):
         pass
-    #TODO:
+    #TO-DO:
     #create a size enforcement protocol.
     #Write in a way to simulate memory limit.

b86714a2f637a5c2a4c24feae3598006bc856163
Luca Guerini <lucaguerini912@gmail.com>
2023-11-06 11:43:05 -0500

    Combined Instruction and Decoder classes into the same file, since both would like to make use of the same data resources.

--- CPU2/Decoder.py
+++ CPU2/Decoder.py
@@ -5,7 +5,7 @@
 #You could call this an assembler compiler,
 #Decoder, or Disassembler depending on your
 #desired nomenclature.
-from . import Instruction, Memory
+from . import Memory
 #Instruction Set is mapped 1:1, so
 #it is possible to use an instruction's mnemonic
 #to find its hex value, and a hex value to find the mnemonic.
@@ -41,9 +41,55 @@ name_to_number = {
 #mnemonic for the operation.
 number_to_name = dict(zip(name_to_number.values(),name_to_number.keys()))
 
+
+class Instruction:
+    #default constructor
+    def __init__(self):
+        self.name = "NOP"
+        self.arguments = []
+        self.size = 0
+    #Non-default constructor.
+    def __init__(self,*args):
+        if len(args) == 0:
+            self.__init__()
+        elif len(args) == 1:
+            self.name = args[0]
+            self.arguments = []
+            self.size = 1
+        else:
+            self.name = args[0]
+            self.arguments = args[1:]
+            self.size = len(args)
+@staticmethod 
+def makeInstruction(str):
+    word = str.split(" ")
+    return Instruction(word)
 class Decoder:
     #fetch: Memory + 
-    def fetch(mem_loc: Memory, location: int):
-        pass
-    def decode(inst: Instruction):
-        pass
+    def fetch(mem_loc: Memory, location: int) -> Instruction:
+        try:
+            return mem_loc[location]
+        except IndexError:
+            pass#IMPLEMENT LATER
+    def decode(inst: Instruction)->(function, list):
+        match inst:
+            case Instruction(name="NOP"):
+                pass
+            case Instruction(name="ADD"):
+                pass
+            case Instruction(name="SUB"):
+                pass
+            case Instruction(name="DIV"):
+                pass
+            case Instruction(name="MUL"):
+                pass
+            case Instruction(name="CMP"):
+                pass
+            case Instruction(name="AND"):
+                pass
+            case Instruction(name="OR"):
+                pass
+            case Instruction(name="NOR"):
+                pass
+            case Instruction(name="XOR"):
+                pass

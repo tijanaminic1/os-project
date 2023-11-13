96d00912eb5957719c6974927d7d7138303733af
tijanaminic1 <63131835+tijanaminic1@users.noreply.github.com>
2023-11-12 23:19:05 -0500

    Revert "update"
    This reverts commit 843d1a78566fae21c185d8b609adcd1424d24cf0.

--- CPU2/CPU.py
+++ CPU2/CPU.py
@@ -1,34 +1,27 @@
 from dataclasses import dataclass
-import threading
-import time
-from ReadyQueue import ReadyQueue, CustomThread
-from Decoder import Instruction, InstructionError, Interrupt
-
-from Registers import Registers
-from Decoder import Decoder
-from Memory import Memory
-
+from . import Registers, Decoder, Interrupt, Memory, ReadyQueue
+from Decoder import Instruction, InstructionError
+from ReadyQueue import CustomThread, Scheduler, ReadyQueue
 @dataclass
 class CPU:
-    registers: 'Registers'
-    decoder: 'Decoder'
-    cache: 'Memory'
-    RAM: 'Memory'
 
-    def __init__(self, registers, decoder, cache, RAM):
-        self.registers = registers
-        self.decoder = decoder
-        self.cache = cache
-        self.RAM = RAM
-        self.ready_queue = ReadyQueue()
-
-    def fetch(self, address) -> Instruction:
+    registers: Registers
+    decoder: Decoder
+    cache: Memory
+    RAM: Memory
+    #fetch: int -> Instruction
+    #purpose: gets the instruction stored at the given memory location
+    def fetch(self, address)-> Instruction:
         return self.decoder.fetch(self.cache, address)
-
-    def decode(self, inst: Instruction) -> tuple:
+    #decode: Instruction -> Tuple
+    #purpose: Decodes an instruction into its mnemonic, 
+    #effects, and locates its operands
+    def decode(self,inst: Instruction)->(str,function,list):
         return self.decoder.decode(inst)
-
-    def interruptHandler(self, i: 'Interrupt') -> None:
+    
+    #InterruptHandler: Interrupt -> Any
+    #Purpose: Handles interrupts
+    def InterruptHandler(i: Interrupt):
         match i:
             case Interrupt(name="PRINT"):
                 print(i.items())
@@ -38,53 +31,75 @@ class CPU:
                 print("DMA failed due to fatal error.")
             case Interrupt(name="DMAIndexOutOfBounds"):
                 print("DMA failed! Entry does not exist.")
-
-    def execute_instruction(self, instruction: tuple) -> None:
-        reglist = ["A", "B", "C", "D", "E", "F", "PC", "SP"]
+    #Execute: Instruction -> EFFECT!
+    #Purpose: Attempts to execute the given instruction, making use
+    #of the Decoder in opcode_parser.py to decode the instruction
+    #EFFECT!: Based off of the given Instruction, code will be executed
+    #that modifies the CPU state relevant to the Instruction's significance
+    #within the opcode vocabulary.
+    def execute(self, instruction: (str,function,list) ):
+        reglist = ["A","B","C","D","E","F","PC","SP"] 
         operands = instruction[2]
         fun = instruction[1]
+        #Check operand1's immediate flag
         immediate = True
-
         if len(operands) > 0 and operands[0] in reglist:
             immediate = False
-
+        
+        #This block translates instructions involving registers.
+        #for example, 'ADD A B' would need to find the register
+        #values of A and B and pass them.
+        #It skips the first value of operands because the first
+        #value of operands would be a destination register or immediate.
+        #Thus if A was 1 and B was 3, it would become:
+        #ADD A 3, or "add 3 to the A register"
+        #Effect!: On matched case, the list is updated such that register references
+        #are passed by their value.
         def pass_values(l: list) -> list:
             a = 1
             while a < len(operands):
                 if l[a] in reglist:
                     l[a] = self.registers.__getitem__(l[a])
-                a = a + 1
-
+                a = a+1
         def execute_instruction():
             if immediate:
-                self.registers.__setitem__("SP", fun(operands))
+                self.registers.__setitem__("SP",fun(operands))
             else:
-                self.registers.__setitem__(operands[0], fun(operands))
+                self.registers.__setitem__(operands[0],fun(operands))
 
         operands = pass_values(operands)
         match instruction[0]:
-            case "JMP" | "JLE" | "JE" | "JGE" | "JG" | "JL" | "JNE":
-                m = fun(self.registers.__getitem__("SP"), operands[0])
+            case "JMP"|"JLE"|"JE"|"JGE"|"JG"|"JL"|"JNE": #handle jumps
+                m = fun(self.registers.__getitem___("SP"),operands[0])
                 if m is not None:
-                    self.registers.__setitem__("PC", m - 1)
-            case "AND" | "OR" | "NOR" | "XOR" | "NAND" | "ADD" | "SUB" | "DIV" | "MUL":
+                    self.registers.__setitem__("PC",m-1)#set the PC to m-1.
+            case "AND"|"OR"|"NOR"|"XOR"|"NAND"|"ADD"|"SUB"|"DIV"|"MUL": #handle boolean statements
                 execute_instruction()
             case "RET":
                 pass
             case "NOP":
-                pass
+                pass #does nothing, as intended.
             case "PRINT":
-                self.interruptHandler(Interrupt(name="PRINT", data=operands[2]))
+                self.interruptHandler(Interrupt(name="PRINT",data=operands[2]))
             case "INPUT":
-                self.interruptHandler(Interrupt(name="INPUT", data=operands[2]))
+                self.interruptHandler(Interrupt(name="INPUT",data=operands[2]))
             case _:
                 raise InstructionError
+        #if we're performing a jump operation do the following:
 
+
+    #run: -> Effect!
+    #Purpose: Executes instructions arriving from
+    #memory, then advances the program counter
+
+    #TO-DO: Both execute and run will likely have to be integrated
+    #to run with Memory, once the class exists and is coded.
     def run(self):
         while True:
-            if not self.ready_queue.is_empty():
-                process = self.ready_queue.dequeue()
-                thread = CustomThread(process, self)
-                thread.start()
-                self.ready_queue.enqueue(process)
-
+            address = self.registers["PC"]
+            try:
+                next_address, instruction = self.decoder.decode(address)
+            except IndexError:
+                break
+            self.registers["PC"] = next_address
+            self.execute(instruction)
--- CPU2/ReadyQueue.py
+++ CPU2/ReadyQueue.py
@@ -1,45 +1,104 @@
-import threading
 import time
+import threading
+import Registers
 
 class ReadyQueue:
     def __init__(self):
-        self.processes = []
+        self.items = []
         self.lock = threading.Lock()
 
-    def enqueue(self, process):
+    def enqueue(self, item):
         with self.lock:
-            self.processes.append(process)
+            self.items.append(item)
 
     def dequeue(self):
         with self.lock:
             if not self.is_empty():
-                return self.processes.pop(0)
+                return self.items.pop(0)
             else:
-                return None
+                raise IndexError("Queue is empty")
 
     def is_empty(self):
         with self.lock:
-            return len(self.processes) == 0
+            return len(self.items) == 0
 
     def size(self):
         with self.lock:
-            return len(self.processes)
+            return len(self.items)
 
 class CustomThread(threading.Thread):
-    def __init__(self, process, cpu):
+    def __init__(self, thread_id, registers, queue, program):
         super().__init__()
-        self.process = process
-        self.cpu = cpu
+        self.thread_id = thread_id
+        self.registers = registers
+        self.queue = queue
+        self.program = program
+        self.instruction_pointer = 0
 
     def run(self):
-        while self.process.has_next_instruction():
+        while self.instruction_pointer < len(self.program):
             try:
-                instruction = self.process.get_next_instruction()
-                self.cpu.execute_instruction(instruction)
-                time.sleep(1)
+                instruction = self.program[self.instruction_pointer]
+                self.execute_instruction(instruction)
+                self.instruction_pointer += 1
+                time.sleep(1)  # Simulate processing time for each instruction
             except KeyboardInterrupt:
-                print(f"Thread for process {self.process} interrupted. Saving state...")
+                print(f"Thread {self.thread_id} interrupted. Saving registers and moving to the back of the queue...")
+                register_values = self.registers.copy()
+                self.registers.save_registers(self.thread_id, register_values)
+                self.queue.enqueue(self.thread_id)
                 break
 
+    def execute_instruction(self, instruction):
+        operation = instruction['operation']
+        operand = instruction['operand']
+
+        if operation == 'ADD':
+            self.registers['A'] += operand
+        elif operation == 'SUB':
+            self.registers['B'] -= operand
+        # Add more operations as needed
+
+# Example usage:
+class Scheduler:
+    def __init__(self):
+        self.queue = ReadyQueue()
+        self.registers = Registers()
+
+    def add_thread(self, thread):
+        self.queue.enqueue(thread)
+
+    def start_scheduling(self):
+        while True:
+            if not self.queue.is_empty():
+                thread = self.queue.dequeue()
+                thread.start()
+                self.queue.enqueue(thread)
+
+# Example programs:
+program1 = [
+    {'operation': 'ADD', 'operand': 10},
+    {'operation': 'SUB', 'operand': 5},
+    {'operation': 'ADD', 'operand': 7},
+    # Add more instructions as needed
+]
+
+program2 = [
+    {'operation': 'SUB', 'operand': 3},
+    {'operation': 'ADD', 'operand': 8},
+    # Add more instructions as needed
+]
+
+# Example usage:
+queue_instance = ReadyQueue()
+scheduler_instance = Scheduler()
+
+thread1 = CustomThread(1, Registers(), queue_instance, program1)
+thread2 = CustomThread(2, Registers(), queue_instance, program2)
+
+scheduler_instance.add_thread(thread1)
+scheduler_instance.add_thread(thread2)
+
+scheduler_instance.start_scheduling()
 
 

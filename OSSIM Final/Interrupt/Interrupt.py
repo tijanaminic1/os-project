d95208fbaa04b9270300520e33a75d918970ffe9
Luca Guerini <lucaguerini912@gmail.com>
2023-12-10 13:25:51 -0500

    Interrupt.py modified such that functionality is altered to work better with the Interrupt Stack.

--- OSSIM Final/Interrupt/Interrupt.py
+++ OSSIM Final/Interrupt/Interrupt.py
@@ -18,22 +18,22 @@ class Interrupt(Exception):
     @property.getter
     def MESSAGE(self):
         return self.message
-    #HANDLE: Interrupt -> Effect!
-    #Purpose: When an interrupt is signalled,
-    #the HANDLE(Interrupt) function defines an
-    #approach to deal with the particular interrupt
-    @staticmethod
-    def HANDLE(interrupt: Interrupt):
-        match interrupt:
-            case Interrupt(name="PRINT"):
-                print(interrupt.MESSAGE())
-            case Interrupt(name="INPUT"):
-                input(interrupt.MESSAGE())
-            case Interrupt(name="DMAFatalError"):#TODO: Estalish recovery protocol for DMA failure.
-                print("DMA failed due to fatal error.")
-            case Interrupt(name="DMAIndexOutOfBounds"): #This is also a type of Page Fault
-                print("DMA failed! Entry does not exist.")
-            case Interrupt(name="ProcessTooLarge"):#TODO: Generally a fatal error, unless we do VRAM.
-                print("Process too large to fit in the cache!")
-            case Interrupt(name="CacheBottleneck"):#TODO: Integrate this with scheduling algorithm. It is a scheduling issue.
-                print("Not enough space to fit"f" {i.data} right now.")
+#HANDLE: Interrupt -> Effect!
+#Purpose: When an interrupt is signalled,
+#the HANDLE(Interrupt) function defines an
+#approach to deal with the particular interrupt
+@staticmethod
+def HANDLE(interrupt: Interrupt):
+    match interrupt:
+        case Interrupt(name="PRINT"):
+            print(interrupt.MESSAGE())
+        case Interrupt(name="INPUT"):
+            input(interrupt.MESSAGE())
+        case Interrupt(name="DMAFatalError"):#TODO: Estalish recovery protocol for DMA failure.
+            print("DMA failed due to fatal error.")
+        case Interrupt(name="DMAIndexOutOfBounds"): #This is also a type of Page Fault
+            print("DMA failed! Entry does not exist.")
+        case Interrupt(name="ProcessTooLarge"):#TODO: Generally a fatal error, unless we do VRAM.
+            print("Process too large to fit in the cache!")
+        case Interrupt(name="CacheBottleneck"):#TODO: Integrate this with scheduling algorithm. It is a scheduling issue.
+            print("Not enough space to fit"f" {i.data} right now.")

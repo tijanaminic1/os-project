e2e4248f9e32f152075f8cb509b8f4a955e3fb54
tijanaminic1 <63131835+tijanaminic1@users.noreply.github.com>
2023-12-11 10:46:12 -0500

    tijana changes

--- FinalProject/Scheduling/RRScheduler.py
+++ FinalProject/Scheduling/RRScheduler.py
@@ -4,28 +4,32 @@ from ..InstructionArchitecture.Process import Process
 
 #https://docs.python.org/3/library/itertools.html
 #cycle
-class RRScheduler(Scheduler):
+class RRScheduler:
     def __init__(self, processes: List[Process] = []):
         self.processes = processes
 
-    def RR(self, processes:List[Process], process:Process, time_quantum: int):
-
-        remaining_time = process.burst_time
+    def RR(self, time_quantum: int):
+        processes = self.processes.copy()  # Make a copy to avoid modifying the original list
         time = 0
         waiting_time = 0
         turnaround_time = 0
 
         while processes:
-            current_process = self.processes[0]
+            current_process = processes[0]
 
             if current_process.remaining_time <= time_quantum:
                 time += current_process.remaining_time
                 turnaround_time += time
                 waiting_time += turnaround_time - current_process.burst_time
+                processes.pop(0)
             else:
                 time += time_quantum
                 current_process.remaining_time -= time_quantum
                 processes.append(current_process)
+                processes.pop(0)
+
+        avg_waiting_time = waiting_time / len(self.processes)
+        avg_turnaround_time = turnaround_time / len(self.processes)
                 
         
 
